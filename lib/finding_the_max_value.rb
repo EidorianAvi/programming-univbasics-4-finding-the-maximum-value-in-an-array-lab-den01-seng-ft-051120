b66fffb7b613e5ada0c0a8157aa51c6f0429161d
Rene Avila <reneavila1993@gmail.com>
2020-05-07 18:51:33 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,14 +1,3 @@
-# def find_max_value(array)
-#   count = 0 
-#   max_value = 0 
-#   while count < array.length do 
-#     if max_value < array[count]
-#       max_value = array[count]
-#     end 
-#     count += 1 
-#   end 
-#   max_value
-# end
 
 def find_max_value(array)
   max_value = 0 
